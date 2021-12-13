2318613e7f7aa556e72e1751c53e1d92099f07ad
David Dunham <daviddfh@gmail.com>
2021-12-13 15:08:50 -0500

    Day 13 Modified. More to come.

--- day13part02.py
+++ day13part02.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+# EXTRACT DATA
 with open('inputday13.txt') as f:
     data = f.read()
 data = data.strip().split('\n')
@@ -11,22 +12,14 @@ instructions = data[div+1:]
 dots = [x.split(',') for x in dots]
 dots = [[int(x), int(y)] for [x, y] in dots]
 
-xMax = 0
-yMax = 0
-for dot in dots:
-    x = dot[0]
-    y = dot[1]
-    if x > xMax:
-        xMax = x
-    if y > yMax:
-        yMax = y
-
+#CREATE PAGE AND FILL WITH DOTS
+xMax = max([dot[0] for dot in dots])
+yMax = max([dot[1] for dot in dots])
 page = np.full(shape=[yMax+1, xMax+1], fill_value='.')
-
 for dot in dots:
     page[dot[1]][dot[0]] = '#'
 
-
+#FOLD FUNCTION DEFINITION
 def fold(axis, degree, page):
     if axis == 'y':
         for y in range(len(page)):
@@ -34,9 +27,9 @@ def fold(axis, degree, page):
             mirror = degree + yDistance
             if mirror >= 0 and mirror <= len(page)-1:
                 for x in range(len(page[y])):
-                    if page[y][x] == '#' or page[mirror][x] == '#':
+                    if page[mirror][x] == '#':
                         page[y][x] = '#'
-                        page[mirror][x] = '#'
+                        #page[mirror][x] = '#' (Implicit. This step skipped because the latter half is always deleted.)
         page = np.delete(page, slice(degree, len(page)), 0)
     else:
         for y in range(len(page)):
@@ -44,13 +37,13 @@ def fold(axis, degree, page):
                 xDistance = degree-x
                 mirror = degree + xDistance
                 if mirror >= 0 and mirror <= len(page[y])-1:
-                    if page[y][x] == '#' or page[y][mirror] == '#':
+                    if page[y][mirror] == '#':
                         page[y][x] = '#'
-                        page[y][mirror] = '#'
+                        #page[y][mirror] = '#'
         page = np.delete(page, slice(degree, len(page[0])), 1)
     return page
 
-
+#COMMIT FOLDS FOR EACH INSTRUCTION
 for instruction in instructions:
     axis = instruction[instruction.index('g')+2]
     degree = int(instruction[instruction.index(axis)+2:])
